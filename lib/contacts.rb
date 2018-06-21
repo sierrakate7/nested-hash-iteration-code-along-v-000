7a3d99f3a653338819fafb077ad7ec2daf9132c3
Sierra McCahon <smitche4@mail.umw.edu>
2018-06-21 20:01:52 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,3 +19,9 @@ contacts.each do |person, contact_details_hash|
   binding.pry
 end
 end
+
+contacts.each do |person, contact_details_hash|
+  contact_details_hash.each do |attribute, data|
+    binding.pry
+  end
+end
