d2cf1bafd3916ec008896db2d41e80f15f8db09c
Sierra McCahon <smitche4@mail.umw.edu>
2018-06-21 20:02:11 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,10 +15,6 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts.each do |person, contact_details_hash|
-  binding.pry
-end
-end
 
 contacts.each do |person, contact_details_hash|
   contact_details_hash.each do |attribute, data|
